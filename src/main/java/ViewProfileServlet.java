76420b56cb23123851cea59c793ceee2b0c08883
Mitchell Arnold <mitchell.arnold2019@gmail.com>
2019-10-22 15:27:52 -0500

    feat: update mvc exercise

--- src/main/java/ViewProfileServlet.java
+++ src/main/java/ViewProfileServlet.java
@@ -11,4 +11,14 @@ public class ViewProfileServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("/profile.jsp").forward(request, response);
     }
+
+
+
+//            if (request.getMethod().equalsIgnoreCase("post")) {
+//        String username = request.getParameter("username");
+//        String password = request.getParameter("password");
+//        if (username.equals("admin") && password.equals("password")) {
+//            response.sendRedirect("/profile");
+//        }
+//    }
 }
