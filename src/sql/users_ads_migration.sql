cae5b4be55e744a04672caa4cb882d4969903de1
Mitchell Arnold <mitchell.arnold2019@gmail.com>
2019-10-24 14:57:21 -0500

    feat: initialize db / user / user_ads_migration

---
+++ src/sql/users_ads_migration.sql
@@ -0,0 +1,17 @@
+USE adlister_db;
+CREATE TABLE users (
+                       id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                       username VARCHAR(50) NOT NULL,
+                       email VARCHAR(100) NOT NULL,
+                       password VARCHAR(50) DEFAULT NULL,
+                       PRIMARY KEY (id)
+                   );
+
+CREATE TABLE ads (
+                       id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                       user_id INT NOT NULL,
+                       title VARCHAR(100) NOT NULL,
+                       description VARCHAR(255) NOT NULL,
+                       PRIMARY KEY (id),
+                       FOREIGN KEY (user_id) REFERENCES users (id)
+);
